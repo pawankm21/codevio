140367a4e05d29f504c2c24869109a969bd02900
the-supernova <biswalabhijit04@gmail.com>
2022-07-13 20:38:59 +0530

    Add create and join room logic (partially working)

--- pages/index.js
+++ pages/index.js
@@ -6,22 +6,53 @@ import { cpp } from '@codemirror/lang-cpp';
 import { python } from '@codemirror/lang-python';
 import { java } from '@codemirror/lang-java';
 import { sublime } from '@uiw/codemirror-theme-sublime';
-import { useCallback } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
+import socketIOClient from "socket.io-client";
 export default function Home() {
   const onChange = useCallback((value, changeValue) => {
     console.log(value);
     console.log(changeValue);
   },[]);
-  
+const SOCKET_SERVER_URL = "http://localhost:4000";
+const socketRef = useRef();
+const handleCreate = () => {
+  socketRef.current.emit("roomAllot", socketRef.id);
+  console.log("Your room id:", socketRef.id);
+}
+const handleJoin = (e) => {
+  e.preventDefault();
+  const roomId = e.target.roomid.value;
+  socketRef.current.emit("roomAllot", roomId);
+  roomId = "";
+}
+useEffect(() => {
+  socketRef.current = socketIOClient(SOCKET_SERVER_URL);
+  socketRef.current.on("connect", () => {
+    console.log("Connected to server");
+  });
+  socketRef.current.on("disconnect", () => {
+    console.log("Disconnected from server");
+  });
+  return () => {
+    socketRef.current.disconnect();
+  };
+}, []);
   return (
     <div>
       <Head>
-        <title></title>
+        <title>Codevio</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main  id="main">
+      <main id="main">
+        <div className="button-row">
+          <input type="button" value="Create Room" onClick={handleCreate} />
+          <form onSubmit={handleJoin}>
+            <input id="roomid" type="text" placeholder="Enter RoomId" />
+            <button>Join Room </button>
+          </form>
+        </div>
         <CodeMirror
           value="console.log('hello world!');"
           height="100vh"
